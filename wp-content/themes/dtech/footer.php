19679bc1726d8f3ecaf8c6fb16acb5d59cddab5a
mohamedrayen <80204476+mohamedrayen@users.noreply.github.com>
2024-04-08 13:59:10 +0100

    dev

--- wp-content/themes/dtech/footer.php
+++ wp-content/themes/dtech/footer.php
@@ -1,1 +1,185 @@
-<?php
+<!-- footer section start -->
+<footer class="xs-footer-section">
+	<!-- feature list section -->
+	<div class="xs-feature-section">
+		<div class="container">
+			<div class="row feature-v2">
+				<div class="col-lg-6">
+					<div class="xs-feature-group wow fadeInLeft">
+						<div class="media">
+							<div class="feature-img d-flex">
+								<img src="assets/images/sheild-2.png" alt="hosting feature img">
+							</div>
+							<div class="media-body feature-content">
+								<h4 class="xs-title">Days money back guarantee</h4>
+								<p>Share processes an data secure lona need <br> to know basis without the need.</p>
+								<div class="xs-btn-wraper">
+									<a href="#" class="btn btn-primary">Know More</a>
+								</div>
+							</div>
+						</div>
+					</div><!-- .xs-feature-group END -->
+				</div>
+				<div class="col-lg-6">
+					<div class="xs-feature-group wow fadeInRight">
+						<div class="media">
+							<div class="feature-img d-flex">
+								<img src="assets/images/cloud-lock-2.png" alt="hosting feature img">
+							</div>
+							<div class="media-body feature-content">
+								<h4 class="xs-title">Build and protect your brand</h4>
+								<p>Share processes an data secure lona need <br> to know basis without the need.</p>
+								<div class="xs-btn-wraper">
+									<a href="#" class="btn btn-primary">Know More</a>
+								</div>
+							</div>
+						</div>
+					</div><!-- .xs-feature-group END -->
+				</div>
+			</div><!-- .row END -->
+		</div><!-- .container END -->
+	</div>			<!-- END feature list section -->
+	<div class="footer-group" style="background-image: url(assets/images/footer-bg.png);">
+		<div class="footer-main">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-md-6">
+						<div class="footer-widget wow fadeInUp">
+							<h3 class="widget-title">Services</h3>
+							<ul class="xs-list">
+								<li><a href="shared-hosting.html">Shared Hosting</a></li>
+								<li><a href="reseller-hosting.html">Reseller Hosting</a></li>
+								<li><a href="vps-hosting.html">VPS Hosting</a></li>
+								<li><a href="cloud-hosting.html">Cloud Hosting</a></li>
+								<li><a href="dedicated-hosting.html">Dedicated Hosting</a></li>
+								<li><a href="domain.html">Domain Name</a></li>
+							</ul><!-- .xs-list END -->
+						</div><!-- .footer-widget END -->
+					</div>
+					<div class="col-lg-3 col-md-6">
+						<div class="footer-widget wow fadeInUp" data-wow-duration="1s">
+							<h3 class="widget-title">Company</h3>
+							<ul class="xs-list">
+								<li><a href="about.html">About us</a></li>
+								<li><a href="contact.html">Contact</a></li>
+								<li><a href="support.html">Support</a></li>
+								<li><a href="blog.html">Latest Blog</a></li>
+								<li><a href="pricing.html">Pricing</a></li>
+								<li><a href="affiliate.html">Affiliate</a></li>
+							</ul><!-- .xs-list END -->
+						</div><!-- .footer-widget END -->
+					</div>
+					<div class="col-lg-3 col-md-6">
+						<div class="footer-widget wow fadeInUp" data-wow-duration="1.3s">
+							<h3 class="widget-title">Solutions</h3>
+							<ul class="xs-list">
+								<li><a href="who-is.html">Who Is</a></li>
+								<li><a href="terms.html">Terms of Service</a></li>
+								<li><a href="privacy.html">Privacy Policy</a></li>
+								<li><a href="faq.html">FAQ</a></li>
+								<li><a href="login.html">Log In</a></li>
+							</ul><!-- .xs-list END -->
+						</div><!-- .footer-widget END -->
+					</div>
+					<div class="col-lg-3 col-md-6">
+						<div class="footer-widget wow fadeInUp" data-wow-duration="1.5s">
+							<h3 class="widget-title">Contact Info</h3>
+							<ul class="contact-info-widget">
+								<li class="media">
+									<img src="assets/images/address-pin.png" class="d-flex" alt="contact icon">
+									<span class="media-body">9/4c, 1010 Avenue, NY, USA</span>
+								</li><!-- .media END -->
+								<li class="media">
+									<img src="assets/images/phone-pin.png" class="d-flex" alt="contact icon">
+									<span class="media-body">+216 92 560 245 (toll free) +216 92 560 245</span>
+								</li><!-- .media END -->
+								<li class="media">
+									<img src="assets/images/email-icon.png" class="d-flex" alt="contact icon">
+									<span class="media-body">9/4c, 1010 Avenue, NY, USA</span>
+								</li><!-- .media END -->
+							</ul><!-- .contact-info-widget -->
+						</div><!-- .footer-widget END -->
+					</div>
+				</div><!-- .row END -->
+			</div><!-- .container END -->
+		</div><!-- .footer-main END -->
+		<div class="container">
+			<div class="footer-bottom">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="footer-bottom-info wow fadeInUp">
+							<p>Offers valid for a limited time onlyand  reflect multi  annual discounts. Other terms and conditions may apply.  <a href="#">Click Here</a></p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<ul class="xs-list list-inline wow fadeInUp" data-wow-duration="1s">
+							<li><img src="assets/images/security/security-company-images-1.png" alt="security company images"></li>
+							<li><img src="assets/images/security/security-company-images-2.png" alt="security company images"></li>
+							<li><img src="assets/images/security/security-company-images-3.png" alt="security company images"></li>
+							<li><img src="assets/images/security/security-company-images-4.png" alt="security company images"></li>
+						</ul>
+					</div>
+				</div><!-- .row END -->
+			</div><!-- .footer-bottom end -->
+		</div><!-- .container end -->
+	</div><!-- .footer-group END -->
+	<div class="footer-copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="copyright-text wow fadeInUp">
+						<p>&copy; 2024 <a href="#">D-Technologie</a> Theme by <a href="https://www.facebook.com/Inespublicitecom">Publicité.com</a></p>
+					</div><!-- .copyright-text END -->
+				</div>
+				<div class="col-md-4">
+					<div class="footer-logo-wraper wow fadeInUp" data-wow-duration="1s">
+						<a href="index.html" class="footer-logo"><img src="assets/images/footer-logo.png" alt="footer logo"></a>
+					</div><!-- .footer-logo-wraper END -->
+				</div>
+				<div class="col-md-4">
+					<div class="social-list-wraper wow fadeInUp" data-wow-duration="1.3s">
+						<ul class="social-list">
+							<li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+							<li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+							<li><a href="#" class="googlePlus"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</div><!-- .social-list-wraper END -->
+				</div>
+			</div><!-- .row END -->
+		</div><!-- .container END -->
+	</div><!-- .footer-copyright END -->
+</footer>
+<!-- footer section end -->
+<!-- js file start -->
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/jquery-ui.min.js"></script>
+<script src="assets/js/plugins.js"></script>
+<script src="assets/js/Popper.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/tweetie.js"></script>
+<script src="assets/js/jquery.parallax-scroll.js"></script>
+<script src="assets/js/jquery.ajaxchimp.min.js"></script>
+<script src="assets/js/jquery.parallax.js"></script>
+<script src="assets/js/hostslide.js"></script>
+<script src="assets/js/vps-slider.js"></script>
+<script src="assets/js/vps-slider-settings.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyCy7becgYuLwns3uumNm6WdBYkBpLfy44k"></script>
+<script src="assets/js/jquery.themepunch.tools.min.js"></script>
+<script src="assets/js/jquery.themepunch.revolution.min.js"></script>
+<script src="assets/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="assets/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="assets/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="assets/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="assets/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="assets/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="assets/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="assets/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="assets/js/extensions/revolution.extension.video.min.js"></script>
+<script src="assets/js/shuffle-letters.js"></script>
+<script src="assets/js/main.js"></script>		<!-- End js file -->
+</body>
+</html>
--- wp-content/themes/dtech/header.php
+++ wp-content/themes/dtech/header.php
@@ -1,1 +1,367 @@
-<?php
+<!-- get_header('Page Name','Title')-->
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<title>D-Technologie - Index Two</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Karla:400,700,700i%7CRubik:300,400,500,700" rel="stylesheet">
+
+	<link rel="icon" type="image/png" href="favicon.ico">
+	<!-- Place favicon.ico in the root directory -->
+	<link rel="apple-touch-icon" href="apple-touch-icon.png">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<link rel="stylesheet" href="assets/css/iconfont.css">
+	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="assets/css/jquery-ui.structure.min.css">
+	<link rel="stylesheet" href="assets/css/jquery-ui.theme.min.css">
+	<link rel="stylesheet" href="assets/css/rev-settings.css">
+
+
+	<!--For Plugins external css-->
+	<link rel="stylesheet" href="assets/css/plugins.css" />
+
+	<!--Theme custom css -->
+	<link rel="stylesheet" href="assets/css/style.css">
+
+	<!--Theme Responsive css-->
+	<link rel="stylesheet" href="assets/css/responsive.css" />
+</head>
+<body>
+<!--[if lt IE 10]>
+<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<!-- prelaoder -->
+<div id="preloader">
+	<div class="preloader-wrapper">
+		<div class="spinner"></div>
+	</div>
+	<div class="preloader-cancel-btn">
+		<a href="#" class="btn btn-secondary prelaoder-btn">Cancel Preloader</a>
+	</div>
+</div>	<!-- END prelaoder -->
+<div class="header-transparent nav-sticky">
+	<!-- topBar section -->
+	<div class="xs-top-bar">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<ul class="xs-top-bar-info">
+						<li>
+							<p><i class="icon icon-phone1"></i>+216 92 560 245</p>
+						</li>
+						<li>
+							<a href="mailto:contact@d-technology.com"><i class="icon icon-envelope1"></i>contact@d-technology.com</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-6">
+					<ul class="top-menu">
+						<li><a href="login.html"><i class="icon icon-key2"></i> Login</a></li>
+						<!-- <li><a href="signup.html">Sign Up</a></li> -->
+						<li><a href="support.html"><i class="icon icon-hours-support"></i> Support</a></li>
+					</ul>
+				</div>
+			</div><!-- .row END -->
+		</div><!-- .container END -->
+	</div>    <!-- End topBar section -->
+
+	<!-- header section -->
+	<header class="xs-header header-style-2">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="xs-logo-wraper">
+					<a href="index.html" class="xs-logo">
+						<img src="assets/images/logo.png" alt="">
+					</a>
+				</div>
+				<nav class="xs-menus ml-auto">
+					<div class="nav-header">
+						<a class="nav-brand" href="index.html"></a>
+						<div class="nav-toggle"></div>
+					</div>
+					<!-- menu list start -->
+					<div class="nav-menus-wrapper">
+						<ul class="nav-menu">
+							<li class="mega-menus"><a href="#">Home</a>
+								<div class="megamenu-panel home-menu-panel">
+									<div class="row">
+										<div class="col-lg-4">
+											<a href="index.html" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-one.jpg" alt="">
+												<h4>Home One</h4>
+											</a>
+										</div>
+										<div class="col-lg-4">
+											<a href="index-v2.html" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-two.jpg" alt="">
+												<h4>Home Two</h4>
+											</a>
+										</div>
+										<div class="col-lg-4">
+											<a href="index-v3.html" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-three.jpg" alt="">
+												<h4>Home Three</h4>
+											</a>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-lg-4">
+											<a href="index-v4.html" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-four.jpg" alt="">
+												<h4>Home Four</h4>
+											</a>
+										</div>
+										<div class="col-lg-4">
+											<a href="index-v5.html" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-five.jpg" alt="">
+												<h4>Home Five</h4>
+											</a>
+										</div>
+										<div class="col-lg-4">
+											<a href="index-v6.html" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-six.jpg" alt="">
+												<h4>Home Six</h4>
+											</a>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-lg-4">
+											<a href="index-v7.html" class="single-menu-panle" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-seven.jpg" alt="">
+												<span class="menu-highlight-badge">NEW</span>
+												<h4>Home Seven</h4>
+											</a>
+										</div>
+										<div class="col-lg-4">
+											<a href="index-v8.html" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-seven.jpg" alt="">
+												<span class="menu-highlight-badge">NEW</span>
+												<h4>Home Eight Rev Hero</h4>
+											</a>
+										</div>
+										<div class="col-lg-4">
+											<a href="index-v9.html" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-nine.jpg" alt="">
+												<span class="menu-highlight-badge">NEW</span>
+												<h4>Home Nine</h4>
+											</a>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-lg-4">
+											<a href="index-v10.html" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-ten.jpg" alt="">
+												<span class="menu-highlight-badge">NEW</span>
+												<h4>Home Ten</h4>
+											</a>
+										</div>
+										<div class="col-lg-4">
+											<a href="index-v11.html" class="single-menu-panle">
+												<img src="assets/images/menu-screenshorts/home-eleven.jpg" alt="">
+												<span class="menu-highlight-badge">NEW</span>
+												<h4>Home Eleven Rev Slider</h4>
+											</a>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li class="mega-menus"><a href="#">Pages</a>
+								<div class="megamenu-panel xs-icon-menu">
+									<div class="row">
+										<div class="col-lg-4">
+											<div class="single-menu-item">
+												<a href="about.html"><i class="icon icon-briefcase"></i>About</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="affiliate.html"><i class="icon icon-line-chart"></i>Affiliate</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="pricing.html"><i class="icon icon-agenda"></i>Pricing</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="faq.html"><i class="icon icon-question-circle"></i>FAQ</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="terms.html"><i class="icon icon-direction"></i>Terms</a>
+											</div>
+										</div>
+										<div class="col-lg-4">
+											<div class="single-menu-item">
+												<a href="who-is.html"><i class="icon icon-folder"></i>Whois</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="support.html"><i class="icon icon-chat"></i>Support</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="cart.html"><i class="icon icon-shopping-cart-1"></i>Cart</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="checkout.html"><i class="icon icon-wallet"></i>Checkout</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="privacy.html"><i class="icon icon-padlock-1"></i>Privacy</a>
+											</div>
+										</div>
+										<div class="col-lg-4">
+											<div class="single-menu-item">
+												<a href="login.html"><i class="icon icon-agenda-1"></i>Login</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="signup.html"><i class="icon icon-users"></i> Signup</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="forgot-password.html"><i class="icon icon-safebox"></i> Forgot password</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="404.html"><i class="icon icon-sad"></i>404</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li class="mega-menus"><a href="#">Hosting</a>
+								<div class="megamenu-panel xs-service-menu">
+									<div class="row">
+										<div class="col-lg-6">
+											<a href="shared-hosting.html">
+												<div class="media">
+													<img src="assets/images/menu-icon/icon-1.png" alt="">
+													<div class="media-body">
+														<h4>Shared Hosting</h4>
+														<p>Early Create sites & offload them to clients</p>
+													</div>
+												</div>
+											</a>
+											<a href="reseller-hosting.html">
+												<div class="media">
+													<img src="assets/images/menu-icon/icon-2.png" alt="">
+													<div class="media-body">
+														<h4>Reseller Hosting</h4>
+														<p>Early Create sites & offload them to clients</p>
+													</div>
+												</div>
+											</a>
+											<a href="vps-hosting.html">
+												<div class="media">
+													<img src="assets/images/menu-icon/icon-3.png" alt="">
+													<div class="media-body">
+														<h4>VPS hosting</h4>
+														<p>Protect your Brand & Deploy faster</p>
+													</div>
+												</div>
+											</a>
+										</div>
+										<div class="col-lg-6">
+											<a href="cloud-hosting.html">
+												<div class="media">
+													<img src="assets/images/menu-icon/icon-4.png" alt="">
+													<div class="media-body">
+														<h4>Cloud Hosting</h4>
+														<p>Access your files from anywhere</p>
+													</div>
+												</div>
+											</a>
+											<a href="dedicated-hosting.html">
+												<div class="media">
+													<img src="assets/images/menu-icon/icon-5.png" alt="">
+													<div class="media-body">
+														<h4>Dedicated Hosting</h4>
+														<p>Be secure, be safe</p>
+													</div>
+												</div>
+											</a>
+											<a href="domain.html">
+												<div class="media">
+													<img src="assets/images/menu-icon/icon-6.png" alt="">
+													<div class="media-body">
+														<h4>Domain Name</h4>
+														<p>Grab your domain name today!</p>
+													</div>
+												</div>
+											</a>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li><a href="#">News</a>
+								<ul class="nav-dropdown xs-icon-menu">
+									<li class="single-menu-item">
+										<a href="blog.html"><i class="icon icon-file-2"></i>Blog</a>
+									</li>
+									<li class="single-menu-item">
+										<a href="blog-2.html"><i class="icon icon-file-2"></i>Blog 2</a>
+									</li>
+									<li class="single-menu-item">
+										<a href="blog-single.html"><i class="icon icon-contract"></i>Blog single</a>
+									</li>
+								</ul>
+							</li>
+							<li><a href="contact.html">Contact</a></li>
+							<li class="mega-menus"><a href="#">Elements</a>
+								<div class="megamenu-panel xs-icon-menu">
+									<div class="row">
+										<div class="col-lg-4">
+											<div class="single-menu-item">
+												<a href="elements-features.html"><i class="icon icon-sun"></i>Feature Box</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="elements-pricing-table.html"><i class="icon icon-database"></i>Pricing Table</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="elements-testimonial.html"><i class="icon icon-lock"></i>Testimonial</a>
+											</div>
+										</div>
+										<div class="col-lg-4">
+											<div class="single-menu-item">
+												<a href="elements-blogpost.html"><i class="icon icon-heart"></i>Blog Post</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="elements-clients.html"><i class="icon icon-printer"></i>Clients</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="elements-team.html"><i class="icon icon-camera"></i>Team</a>
+											</div>
+										</div>
+										<div class="col-lg-4">
+											<div class="single-menu-item">
+												<a href="elements-domain-search.html"><i class="icon icon-phone"></i>Domain Search</a>
+											</div>
+											<div class="single-menu-item">
+												<a href="elements-calltoaction.html"><i class="icon icon-construction"></i>Call To Action</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li><a href="http://whmcs.finesttheme.com/" target="_blank">Whmcs</a></li>
+						</ul>
+					</div>                <!-- menu list end -->
+					<ul class="xs-menu-tools">
+						<li>
+							<a href="#modal-popup-1" class="languageSwitcher-button xs-modal-popup"><i class="icon icon-internet"></i></a>
+						</li>
+						<li>
+							<a href="#" class="offset-side-bar"><i class="icon icon-cart1"></i><span class="item-count">2</span></a>
+						</li>
+						<li>
+							<a href="#modal-popup-2" class="navsearch-button xs-modal-popup"><i class="icon icon-search"></i></a>
+						</li>
+						<li>
+							<a href="#" class="navSidebar-button"><i class="icon icon-burger-menu"></i></a>
+						</li>
+					</ul>
+				</nav>
+			</div><!-- .row END -->
+		</div><!-- .container END -->
+	</header>    <!-- End header section -->
+</div>
