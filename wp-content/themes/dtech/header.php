2c19741564bd73b093cd6e0c18706267b3751b5f
mohamedrayen <80204476+mohamedrayen@users.noreply.github.com>
2024-04-24 01:15:26 +0100

    dev

--- wp-content/themes/dtech/header.php
+++ wp-content/themes/dtech/header.php
@@ -13,7 +13,7 @@
 	<link rel="icon" type="image/png" href="favicon.ico">
 	<!-- Place favicon.ico in the root directory -->
 	<link rel="apple-touch-icon" href="apple-touch-icon.png">
-	<link rel="stylesheet" href="<?= get_ stylesheet_directory_uri() ?>/assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?= get_stylesheet_directory_uri() ?>/assets/css/font-awesome.min.css">
 
 	<link rel="stylesheet" href="<?= get_stylesheet_directory_uri() ?>/assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="<?= get_stylesheet_directory_uri() ?>/assets/css/animate.css">
@@ -66,9 +66,11 @@
 				</div>
 				<div class="col-md-6">
 					<ul class="top-menu">
-						<li><a href="login.html"><i class="icon icon-key2"></i> Login</a></li>
-						<!-- <li><a href="signup.html">Sign Up</a></li> -->
-						<li><a href="support.html"><i class="icon icon-hours-support"></i> Support</a></li>
+                        <li><a href="#" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#" class="instagram"><i class="fa fa-instagram"></i></a></li>
+                        <li><a href="#" class="googlePlus"><i class="fa fa-google-plus"></i></a></li>
 					</ul>
 				</div>
 			</div><!-- .row END -->
@@ -92,274 +94,83 @@
 					<!-- menu list start -->
 					<div class="nav-menus-wrapper">
 						<ul class="nav-menu">
-							<li class="mega-menus"><a href="#">Home</a>
-								<div class="megamenu-panel home-menu-panel">
-									<div class="row">
-										<div class="col-lg-4">
-											<a href="index.html" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-one.jpg" alt="">
-												<h4>Home One</h4>
-											</a>
-										</div>
-										<div class="col-lg-4">
-											<a href="index-v2.html" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-two.jpg" alt="">
-												<h4>Home Two</h4>
-											</a>
-										</div>
-										<div class="col-lg-4">
-											<a href="index-v3.html" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-three.jpg" alt="">
-												<h4>Home Three</h4>
-											</a>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-lg-4">
-											<a href="index-v4.html" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-four.jpg" alt="">
-												<h4>Home Four</h4>
-											</a>
-										</div>
-										<div class="col-lg-4">
-											<a href="index-v5.html" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-five.jpg" alt="">
-												<h4>Home Five</h4>
-											</a>
-										</div>
-										<div class="col-lg-4">
-											<a href="index-v6.html" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-six.jpg" alt="">
-												<h4>Home Six</h4>
-											</a>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-lg-4">
-											<a href="index-v7.html" class="single-menu-panle" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-seven.jpg" alt="">
-												<span class="menu-highlight-badge">NEW</span>
-												<h4>Home Seven</h4>
-											</a>
-										</div>
-										<div class="col-lg-4">
-											<a href="index-v8.html" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-seven.jpg" alt="">
-												<span class="menu-highlight-badge">NEW</span>
-												<h4>Home Eight Rev Hero</h4>
-											</a>
-										</div>
-										<div class="col-lg-4">
-											<a href="index-v9.html" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-nine.jpg" alt="">
-												<span class="menu-highlight-badge">NEW</span>
-												<h4>Home Nine</h4>
-											</a>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-lg-4">
-											<a href="index-v10.html" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-ten.jpg" alt="">
-												<span class="menu-highlight-badge">NEW</span>
-												<h4>Home Ten</h4>
-											</a>
-										</div>
-										<div class="col-lg-4">
-											<a href="index-v11.html" class="single-menu-panle">
-												<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-screenshorts/home-eleven.jpg" alt="">
-												<span class="menu-highlight-badge">NEW</span>
-												<h4>Home Eleven Rev Slider</h4>
-											</a>
-										</div>
-									</div>
-								</div>
+
+							<li class="mega-menus">
+                                <a href="about.php">Qui Somme Nous</a>
 							</li>
-							<li class="mega-menus"><a href="#">Pages</a>
-								<div class="megamenu-panel xs-icon-menu">
-									<div class="row">
-										<div class="col-lg-4">
-											<div class="single-menu-item">
-												<a href="about.html"><i class="icon icon-briefcase"></i>About</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="affiliate.html"><i class="icon icon-line-chart"></i>Affiliate</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="pricing.html"><i class="icon icon-agenda"></i>Pricing</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="faq.html"><i class="icon icon-question-circle"></i>FAQ</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="terms.html"><i class="icon icon-direction"></i>Terms</a>
-											</div>
-										</div>
-										<div class="col-lg-4">
-											<div class="single-menu-item">
-												<a href="who-is.html"><i class="icon icon-folder"></i>Whois</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="support.html"><i class="icon icon-chat"></i>Support</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="cart.html"><i class="icon icon-shopping-cart-1"></i>Cart</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="checkout.html"><i class="icon icon-wallet"></i>Checkout</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="privacy.html"><i class="icon icon-padlock-1"></i>Privacy</a>
-											</div>
-										</div>
-										<div class="col-lg-4">
-											<div class="single-menu-item">
-												<a href="login.html"><i class="icon icon-agenda-1"></i>Login</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="signup.html"><i class="icon icon-users"></i> Signup</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="forgot-password.html"><i class="icon icon-safebox"></i> Forgot password</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="404.html"><i class="icon icon-sad"></i>404</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</li>
-							<li class="mega-menus"><a href="#">Hosting</a>
-								<div class="megamenu-panel xs-service-menu">
-									<div class="row">
-										<div class="col-lg-6">
-											<a href="shared-hosting.html">
-												<div class="media">
-													<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-1.png" alt="">
-													<div class="media-body">
-														<h4>Shared Hosting</h4>
-														<p>Early Create sites & offload them to clients</p>
-													</div>
-												</div>
-											</a>
-											<a href="reseller-hosting.html">
-												<div class="media">
-													<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-2.png" alt="">
-													<div class="media-body">
-														<h4>Reseller Hosting</h4>
-														<p>Early Create sites & offload them to clients</p>
-													</div>
-												</div>
-											</a>
-											<a href="vps-hosting.html">
-												<div class="media">
-													<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-3.png" alt="">
-													<div class="media-body">
-														<h4>VPS hosting</h4>
-														<p>Protect your Brand & Deploy faster</p>
-													</div>
-												</div>
-											</a>
-										</div>
-										<div class="col-lg-6">
-											<a href="cloud-hosting.html">
-												<div class="media">
-													<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-4.png" alt="">
-													<div class="media-body">
-														<h4>Cloud Hosting</h4>
-														<p>Access your files from anywhere</p>
-													</div>
-												</div>
-											</a>
-											<a href="dedicated-hosting.html">
-												<div class="media">
-													<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-5.png" alt="">
-													<div class="media-body">
-														<h4>Dedicated Hosting</h4>
-														<p>Be secure, be safe</p>
-													</div>
-												</div>
-											</a>
-											<a href="domain.html">
-												<div class="media">
-													<img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-6.png" alt="">
-													<div class="media-body">
-														<h4>Domain Name</h4>
-														<p>Grab your domain name today!</p>
-													</div>
-												</div>
-											</a>
-										</div>
-									</div>
-								</div>
-							</li>
-							<li><a href="#">News</a>
-								<ul class="nav-dropdown xs-icon-menu">
-									<li class="single-menu-item">
-										<a href="blog.html"><i class="icon icon-file-2"></i>Blog</a>
-									</li>
-									<li class="single-menu-item">
-										<a href="blog-2.html"><i class="icon icon-file-2"></i>Blog 2</a>
-									</li>
-									<li class="single-menu-item">
-										<a href="blog-single.html"><i class="icon icon-contract"></i>Blog single</a>
-									</li>
-								</ul>
-							</li>
-							<li><a href="contact.html">Contact</a></li>
-							<li class="mega-menus"><a href="#">Elements</a>
-								<div class="megamenu-panel xs-icon-menu">
-									<div class="row">
-										<div class="col-lg-4">
-											<div class="single-menu-item">
-												<a href="elements-features.html"><i class="icon icon-sun"></i>Feature Box</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="elements-pricing-table.html"><i class="icon icon-database"></i>Pricing Table</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="elements-testimonial.html"><i class="icon icon-lock"></i>Testimonial</a>
-											</div>
-										</div>
-										<div class="col-lg-4">
-											<div class="single-menu-item">
-												<a href="elements-blogpost.html"><i class="icon icon-heart"></i>Blog Post</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="elements-clients.html"><i class="icon icon-printer"></i>Clients</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="elements-team.html"><i class="icon icon-camera"></i>Team</a>
-											</div>
-										</div>
-										<div class="col-lg-4">
-											<div class="single-menu-item">
-												<a href="elements-domain-search.html"><i class="icon icon-phone"></i>Domain Search</a>
-											</div>
-											<div class="single-menu-item">
-												<a href="elements-calltoaction.html"><i class="icon icon-construction"></i>Call To Action</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</li>
-							<li><a href="http://whmcs.finesttheme.com/" target="_blank">Whmcs</a></li>
+							<li class="mega-menus"><a href="Managed-Services.php">Infogérence</a>
+                            </li>
+                            <li class="mega-menus"><a href="#">Nos Solutions</a>
+								 <div class="megamenu-panel xs-service-menu">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <a href="Backup.php">
+                                                <div class="media">
+                                                    <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-1.png" alt="">
+                                                    <div class="media-body">
+                                                        <h4>Sauvegarde</h4>
+                                                        <p>Sécurise tes données contre pertes et corruptions.</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="reseller-hosting.php">
+                                                <div class="media">
+                                                    <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-2.png" alt="">
+                                                    <div class="media-body">
+                                                        <h4>Hébergement Web</h4>
+                                                        <p>Lance ton site internet rapidement et facilement.</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="vps-hosting.php">
+                                                <div class="media">
+                                                    <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-3.png" alt="">
+                                                    <div class="media-body">
+                                                        <h4>Messagerie</h4>
+                                                        <p>Communique de manière professionnelle et fiable.</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <a href="cloud-hosting.php">
+                                                <div class="media">
+                                                    <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-4.png" alt="">
+                                                    <div class="media-body">
+                                                        <h4>Cloud Computing</h4>
+                                                        <p>Puissance informatique à la demande, sans matériel.</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="dedicated-hosting.php">
+                                                <div class="media">
+                                                    <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-5.png" alt="">
+                                                    <div class="media-body">
+                                                        <h4>Public Cloud</h4>
+                                                        <p>Stockage et applications en ligne, accessibles partout.</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="domain.php">
+                                                <div class="media">
+                                                    <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/menu-icon/icon-6.png" alt="">
+                                                    <div class="media-body">
+                                                        <h4>VPS</h4>
+                                                        <p>Contrôle serveur dédié, coût inférieur, flexibilité élevée.</p>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="mega-menus"><a href="Network-and-Telecom.php">Réseau et Télecom</a>
+                            </li>
+							<li><a href="contact.php">Contact</a></li>
+
 						</ul>
 					</div>                <!-- menu list end -->
-					<ul class="xs-menu-tools">
-						<li>
-							<a href="#modal-popup-1" class="languageSwitcher-button xs-modal-popup"><i class="icon icon-internet"></i></a>
-						</li>
-						<li>
-							<a href="#" class="offset-side-bar"><i class="icon icon-cart1"></i><span class="item-count">2</span></a>
-						</li>
-						<li>
-							<a href="#modal-popup-2" class="navsearch-button xs-modal-popup"><i class="icon icon-search"></i></a>
-						</li>
-						<li>
-							<a href="#" class="navSidebar-button"><i class="icon icon-burger-menu"></i></a>
-						</li>
-					</ul>
+
 				</nav>
 			</div><!-- .row END -->
 		</div><!-- .container END -->
